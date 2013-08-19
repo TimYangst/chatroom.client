430845ed8f069d59b807d13cd86b66cedf692b7b
Ting Yang <t-tiyan@microsoft.com>
2013-08-19 18:11:38 +0800

    add message front

---
+++ chatroom.client/ViewModel/ChatRoomViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.ComponentModel;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
+namespace chatroom.client.ViewModel
+{
+	public class ChatRoomViewModel : WorkspaceViewModel
+	{
+		public ChatRoomViewModel()
+		{
+			
+		}
+        private string _message = "";
+        public string Message {
+            get {
+                return _message;
+            }
+            set {
+                if (_message == value) return;
+                _message = value;
+                RaisePropertyChanged("Message");
+            }
+        }
+
+        private void sendMessage()
+        {
+            if (this.Message == "")
+            {
+
+            }
+            else
+            {
+                this.Message = "";
+                Console.Out.WriteLine(this.Message);
+            }
+        }
+
+        private ICommand _sendMessage = null;
+        public ICommand SendMessage {
+            get {
+                if (_sendMessage == null) _sendMessage = new RelayCommand(
+                    this.sendMessage
+                    );
+                return _sendMessage;
+            }
+        }
+
+		
+	}
+}
---
+++ chatroom.client/Views/ChatRoomView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace chatroom.client
+{
+	/// <summary>
+	/// ChatRoomView.xaml 的交互逻辑
+	/// </summary>
+	public partial class ChatRoomView : UserControl
+	{
+		public ChatRoomView()
+		{
+			this.InitializeComponent();
+			
+		    
+		}
+	}
+}
