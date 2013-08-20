71665e0761a3e43294b82c8062da2870bff0f88f
Ting Yang <t-tiyan@microsoft.com>
2013-08-20 15:59:42 +0800

    add dll dynamic load logic

---
+++ chatroom.client.console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace chatroom.client.console
+{
+    class DllLoadTester
+    {
+        [DllImport("D:\\project\\chatroom\\dll\\Project.dll")]
+        public static extern int func(int a);
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DllLoadTester.func(3));
+            Console.ReadKey();
+        }
+    }
+}
