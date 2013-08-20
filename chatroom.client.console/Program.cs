a9071c27b474637196c6900230f5789f60dca33d
Ting Yang <t-tiyan@microsoft.com>
2013-08-20 18:08:03 +0800

    modify to string test

--- chatroom.client.console/Program.cs
+++ chatroom.client.console/Program.cs
@@ -7,18 +7,26 @@ using System.Runtime.InteropServices;
 
 namespace chatroom.client.console
 {
-    class DllLoadTester
+    class NumberDllLoadTester
     {
-        [DllImport("D:\\project\\chatroom\\dll\\Project.dll")]
+        [DllImport("D:\\project\\chatroom\\dll\\number\\Project.dll")]
         public static extern int func(int a);
 
     }
-
+    class StringTypeTester
+    {
+        [DllImport("D:\\project\\chatroom\\dll\\string\\Project.dll")]
+        public static extern string conj(string a, string b);
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DllLoadTester.func(3));
+            string a = "12345";
+            string b = "678910";
+            Console.WriteLine(StringTypeTester.conj(a,b));
+            Console.WriteLine(a);
+            Console.WriteLine(b);
             Console.ReadKey();
         }
     }
