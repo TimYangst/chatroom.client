4fb9542983ff39f058cc91d67c6e1f63c55bfefd
Ting Yang <t-tiyan@microsoft.com>
2013-08-23 16:25:56 +0800

    add call function

--- chatroom.client/ViewModel/ChatRoomViewModel.cs
+++ chatroom.client/ViewModel/ChatRoomViewModel.cs
@@ -5,6 +5,9 @@ using System.Windows.Data;
 using System.ComponentModel;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using chatroom.client.Model;
+using System.Runtime.Remoting.Messaging;
 
 namespace chatroom.client.ViewModel
 {
@@ -12,8 +15,42 @@ namespace chatroom.client.ViewModel
 	{
 		public ChatRoomViewModel()
 		{
-			
+            CallViaDelegate();
 		}
+
+
+        private string fetchMessageList()
+        {
+            return "[{\"username\":\"t-tiyan\",\"content\":\"hello!\",\"time\":1377241460939,\"_id\":\"521709744048d4342e000001\",\"__v\":0}]";
+        }
+
+
+        private void CallViaDelegate()
+        {
+            Func<string> func = fetchMessageList;
+            func.BeginInvoke(CallViaDelegateCallback, null);
+
+        }
+
+        private void CallViaDelegateCallback( IAsyncResult ar)
+        {
+            string str = (string)ar.AsyncState;
+            Func<string> func = (ar as AsyncResult).AsyncDelegate as Func<string>;
+            try
+            {
+                string rst = func.EndInvoke(ar);
+                Console.WriteLine(string.Format("{0} => {1}", str, rst));
+                if (rst != null) {
+                    this.MessageList.Add(new Message("12334","yt"));
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(string.Format("{0} => Error: {1}", str, ex.Message));
+            }
+        }
+
+        public ObservableCollection<Message> MessageList = new ObservableCollection<Message>();
+
         private string _message = "";
         public string Message {
             get {
@@ -34,8 +71,8 @@ namespace chatroom.client.ViewModel
             }
             else
             {
-                this.Message = "";
                 Console.Out.WriteLine(this.Message);
+                this.Message = "";
             }
         }
 
